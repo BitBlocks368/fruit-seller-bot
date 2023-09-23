553d7a8d02e3da88ec51f73e18b2e212e7e2667a
Scott Collins <konglin@protonmail.com>
2023-09-23 22:14:05 +1000

    Added further //commentary to event listener

--- fruit-seller-bot.js
+++ fruit-seller-bot.js
@@ -1,18 +1,18 @@
 // Load dotenv package, call config method. 
 // Used to read the .env file in project root.
 // Loads .env values as environment variables.
-// As a result, you can access variables defined in the .env file
+// Access variables defined in the .env file
 // via process.env.VARIABLE_NAME.
 require('dotenv').config();
 
 // Import Client and Intents classes from discord.js library.
-// The Client class is used to interact with the Discord API,
+// Client class used to interact with the Discord API,
 // while the Intents class is used to specify which events
-// your bot should receive from Discord.
+// the bot should receive from Discord.
 const { Client, Intents } = require('discord.js');
 
 // Create new Intents object and assign to the
-// constant myIntents. This object is constructed with an
+// constant intents. This object is constructed with an
 // array of intent flags that specify the types of events
 // the bot client should listen for:
 // Intents.FLAGS.GUILDS allows the bot to receive events related to
@@ -25,11 +25,8 @@ const { Client, Intents } = require('discord.js');
      Intents.FLAGS.GUILDS,
      Intents.FLAGS.GUILD_MESSAGES
  ]);
-
-// const intents = new Intents();
-// intents.add(Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES);
-
-// Create new instance of the Client class, using the myIntents object
+ 
+// Create new instance of the Client class, using the intents object
 // created earlier to inform Discord which events the bot is 
 // interested in receiving. This client object is your gateway to
 // interacting with the Discord API. It can be used to log in to
@@ -49,44 +46,34 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}`);
 });
 
-// Set event listener for the message event.
-// Emitted every time a new message is sent in any channel.
-// Bot can view the message.
-// The function checks:
-// 1. If author of message is a bot (message.author.bot).
-// If it is, it immediately exits to prevent responding to other bots.
-// 2. If the message content, when converted to lowercase,
-// exactly matches the string 'gm'. 
-// If it does, the bot sends 'GM' to the same channel
-// where the original message was sent.
-// client.on('messageCreate', message => {
-    // console.log(`Received message: ${message.content} from ${message.author.tag}`);
-    // console.log(`Message content: ${message.content}`); // Logging only the message content
-    // if (message.author.bot) return; // Ignore messages from bots
-    // if (message.content.toLowerCase() === 'gm') {
-    //     message.channel.send('GM'); // Send 'GM' to the same channel where the message was received
-    // }
-// });
-
- client.on('messageCreate', (message) => {
-     if (message.author.bot) return; // Ignore messages from bots
-     
-     if (message.content) {
-         console.log(`Received message: ${message.content} from ${message.author.tag}`);
-     } else {
-         console.log(`Received a message with no text content from ${message.author.tag}`);
-         if (message.attachments.size > 0) {
-             console.log('Message has attachments:', message.attachments);
-         }
-         if (message.embeds.length > 0) {
-             console.log('Message has embeds:', message.embeds);
-         }
-     }
-     
-     if (message.content && message.content.toLowerCase() === 'gm') {
-         message.channel.send('GM'); // Send 'GM' to the same channel where the message was received
-     }
- });
+// Event listener for the 'messageCreate' event, which is emitted every time
+// a new message is sent in any channel the bot has access to. 
+client.on('messageCreate', (message) => {
+    // 1. Ignore messages from bots to prevent responding to other bots
+    //    which could potentially lead to infinite loops of messages between bots.
+    if (message.author.bot) return;
+    // 2. Log the received message to the console, specifying whether the message
+    //    has text content, attachments, or embeds.
+    if (message.content) {
+        console.log(`Received message: ${message.content} from ${message.author.tag}`);
+    } else {
+        console.log(`Received a message with no text content from ${message.author.tag}`);    
+        // Check if the message has attachments and log them if they exist
+        if (message.attachments.size > 0) {
+            console.log('Message has attachments:', message.attachments);
+        }
+        // Check if the message has embeds and log them if they exist
+        if (message.embeds.length > 0) {
+            console.log('Message has embeds:', message.embeds);
+        }
+    }
+    // 3. Respond to specific messages:
+    //    If the content of the received message, converted to lowercase, matches the string 'gm', 
+    //    the bot sends a 'GM' message to the same channel where the original message was received.
+    if (message.content && message.content.toLowerCase() === 'gm') {
+        message.channel.send('GM');
+    }
+});
 
 // Log client (bot) into Discord using the token stored in the
 // TOKEN constant. This begins the connection process and
