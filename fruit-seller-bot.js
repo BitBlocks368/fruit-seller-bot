0d4dda391219f6d11595de9194d67b6c4ada9b25
Scott Collins <konglin@protonmail.com>
2023-09-26 15:37:55 +1000

    Added story telling aspect. Refined the GM structure to provide replies

--- fruit-seller-bot.js
+++ fruit-seller-bot.js
@@ -16,7 +16,14 @@ const fs = require('fs');
 const gmResponses = JSON.parse(fs.readFileSync('good-morrow-messages.json', 'utf8'));
 const gmRepeatResponses = JSON.parse(fs.readFileSync('repeated-good-morrow-messages.json', 'utf8'));
 const positiveAffirmations = JSON.parse(fs.readFileSync('positiveAffirmations.json', 'utf8'));
+const morningRoutineStoryData = JSON.parse(fs.readFileSync('morningRoutine.json', 'utf8'));
 
+// Function to get a random story sentence
+function getRandomStorySentence() {
+    const sentenceA = morningRoutineStoryData.sentenceA[Math.floor(Math.random() * morningRoutineStoryData.sentenceA.length)];
+    const sentenceB = morningRoutineStoryData.sentenceB[Math.floor(Math.random() * morningRoutineStoryData.sentenceB.length)];
+    return `${sentenceA} ${sentenceB}`;
+}
 // Create new Intents object and assign to the
 // constant intents. This object is constructed with an
 // array of intent flags that specify the types of events
@@ -51,8 +58,7 @@ let db = new sqlite3.Database('./fruit-seller-bot.db', (err) => {
     }
     console.log('Connected to the users database.');
   });
-  
-  // Create a table to store user IDs and timestamps.
+    // Create a table to store user IDs and timestamps.
   db.run(`CREATE TABLE IF NOT EXISTS users (userId TEXT PRIMARY KEY, timestamp INTEGER, state INTEGER DEFAULT 0, warned INTEGER)`);
 
 // Set event listener for the ready event.
@@ -68,7 +74,7 @@ let gmCounter = 0;
 // a new message is sent in any channel the bot has access to. 
 client.on('messageCreate', message => {
     if (message.author.bot) return; // Ignore messages from bots
-    
+
     if (message.content) {
         console.log(`Received message: ${message.content}. From ${message.author.tag}`);
     } else {
@@ -84,30 +90,27 @@ client.on('messageCreate', message => {
     if (/^gm/i.test(message.content)) {
         const userId = message.author.id;
         const now = Date.now();
-         
         db.get(`SELECT timestamp, state FROM users WHERE userId = ?`, [userId], (err, row) => {
             if (err) {
                 return console.error(err.message);
             }
-            
             const twentyFourHours = 24 * 60 * 60 * 1000;
             const lastTimestamp = row ? row.timestamp : 0;
             const state = row ? row.state : 0;
-            
             if (now - lastTimestamp >= twentyFourHours || !row) {
-                message.channel.send(`GM <@${message.author.id}>! ${gmResponses[gmCounter % gmResponses.length]}`);
+                message.reply(`GM <@${message.author.id}>! ${gmResponses[gmCounter % gmResponses.length]}`);
                 gmCounter++;
                 db.run(`REPLACE INTO users (userId, timestamp, state) VALUES (?, ?, ?)`, [userId, now, 1]);
             } else {
                 switch (state) {
                     case 1: // Initial GM state
                         if (message.content.toLowerCase().includes(`gm <@${client.user.id}>`)) {
-                            message.channel.send(positiveAffirmations[Math.floor(Math.random() * positiveAffirmations.length)]);
+                            message.reply(positiveAffirmations[Math.floor(Math.random() * positiveAffirmations.length)]);
                             db.run(`UPDATE users SET state = 2 WHERE userId = ?`, [userId]);
                         }
                         break;
                     case 2: // GM targeted at bot state
-                        message.channel.send(gmRepeatResponses[gmCounter % gmRepeatResponses.length]);
+                        message.reply(gmRepeatResponses[gmCounter % gmRepeatResponses.length]);
                         gmCounter++;
                         db.run(`UPDATE users SET state = 3 WHERE userId = ?`, [userId]);
                         break;
@@ -118,7 +121,50 @@ client.on('messageCreate', message => {
             }
         });
     }
+
+    // The "tell me about your day" mention check
+    if (message.mentions.users.has(client.user.id)) {
+        const withoutMention = message.content.replace(/<@!?[0-9]+>/, '').trim();
+        if (withoutMention.toLowerCase() === 'tell me about your day') {
+            message.reply(`I'll tell you a little of my day. ${getRandomStorySentence()}`);
+        }
+    }
 });
+
+// client.once('ready', () => {
+//    
+//     console.log('Bot is ready!');    // Fetch the guild by ID (replace YOUR_GUILD_ID with the actual guild ID)
+//     const guild = client.guilds.cache.get('863981449912123393');
+//
+//     Check if the guild exists
+//     
+//     if (guild) {
+//         // Get a list of all channels in the guild
+//         const channels = guild.channels.cache.map(channel => `${channel.name} (${channel.id})`);
+//         // Log the list of channels
+//         console.log('List of channels:', channels.join(', '));
+//         }
+//    
+//     // Set an interval to send a message every 10 sec
+//     const tenSec = 10 * 1000; 
+//     setInterval(() => {
+//         const channelName = 'the-moss'; 
+//         const channel = guild.channels.cache.find(ch => ch.name === channelName && ch.type === 'text');
+// 
+//         if (channel) {
+//             console.log('Found the channel:', channelName);
+//             
+//             if (channel.isText()) {
+//                 channel.send(getRandomStorySentence());
+//                 console.log('Message sent to the channel.');
+//             } else {
+//                 console.log('The channel is not a text channel.');
+//            }
+//        } else {
+//             console.log(`Channel with name ${channelName} not found.`);
+//         }
+//     }, tenSec);
+// });
 
 // Log client (bot) into Discord using the token stored in the
 // TOKEN constant. This begins the connection process and
